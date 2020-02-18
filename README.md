08c83644131f28d9286ed4a07cbd65f774a36a18
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-02-19 01:07:05 +0900

    update

--- README.md
+++ README.md
@@ -1,6 +1,117 @@
-# PliersCover.js: Pliers cover generator for Javascript
+# PliersCover JavaScript
+
+工具カバーのレザークラフト用図面を自動作成 (Inkscape extension)
+
+![工具の写真と重ねる](docs/sample2.png)
+
+## 概要
+
+ラジオペンチやニッパーなどの工具カバーのレザークラフト用図面を自動作成する
+Inkscapeエクステンション(拡張機能)です。
+
+各部分のサイズを入力すると、
+レーザーカッターなどで利用できる図面を自動作成します。
+
+(もちろん、紙に印刷して、従来通りの手作業も可能。)
+
+ブラウザの画面上で確認しながらサイズを調整できます。
+
+針穴は菱目で、位置は、必ず角に来るように自動的に調整されます。
+
+
+## 1. インストール
+
+1. ダウンロード
+2. ファイルのコピー
+
+### 1.1 ダウンロード
+
+サイトのURL
+
+[https://github.com/ytani01/PliersCover.js/](https://github.com/ytani01/PliersCover.js/)
+
+ZIP形式のファイルをダウンロードして展開するか、
+``git``に慣れている方は、慣れた方法でリポジトリをクローンしてください。
+
+#### 1.1.1 ZIPファイルをダウンロードして展開する方法
+
+![](docs/github1.png)
+
+
+#### 1.1.2 Gitリポジトリをクローン(clone)する方法
+
+* 通常のやり方
+
+```bash
+$ git clone https://github.com/ytani01/PlierCover.js.git
+```
+
+* githubにsshの設定をしている場合
+
+```bash
+$ git clone git@github.com:ytani01/PlierCover.js.git
+```
+
+
+### 1.2 ファイルのコピー
+
+以下の２つのファイルを所定のフォルダにコピーする。
+
+コピーするファイル
+* plier_cover.html
+* plier_cover.js
+
+適当なフォルダ(ディレクトリ)にコピーして、
+``plier_cover.html``をブラウザで開いて下さい。
+
+
+## 2. 使い方
+
+* 入力欄にサイズを入力
+
+![Inkscape extension: 工具カバー](docs/sample1.png)
+
+* 入力欄のどれでも変更すると、その場で直ちに、描画されます。
+
+* ``[Create SVG]``ボタンでも表示できます。
+
+* ``[Download SVG]``ボタンで、SVGファイルに保存することができます。
+
+
+### 2.1 注意・コツなど
+
+* 角に、針穴が来るように自動調節されるので、
+辺ごとに針穴の間隔が多少不揃いになることがあります。
+縫いしろや針穴のパラメータを微調整調整して、
+針穴が均等になるようにしましょう。
+
+
+
+## A. Memo
+
+### A.1 SVG パスのコマンド
+
+```
+(大文字: 絶対座標、小文字: 相対座標)
+
+M: moveto (始点座標)
+L: lineto
+  H: 水平
+  V: 垂直
+C: curveto: "{C|c} c1x,c1y c2x,c2y x,y"
+  (c1x, c1y), (c2x, c2y): 制御点
+  x,y: 終点
+
+  * 正円(半円)の場合、制御点までの距離は、直径の約0,75倍(?)
+Z: closepath
+```
+
+```
+ex: d="M 50,60 H 60 l 20,50 Z"
+```
+
 
 ## B. 参考
 
 * [Kerf Check Parts Generator](http://doyolab.net/appli/kerf_check/kerf_check.html)
-* [PlierCover: inkscape extension](https://github.com/ytani01/PliersCover)
+* [PlierCover](https://github.com/ytani01/PliersCover)
--- pliers_cover.js
+++ pliers_cover.js
@@ -208,7 +208,7 @@ class SvgPart1Outline extends SvgPath {
   }
 }
 
-class SvgNeedleHole extends SvgObj {
+class SvgNeedleHole extends SvgPolygon {
   constructor(parent, w, h, tf) {
     super(parent, []);
     this.w = w;
@@ -219,10 +219,10 @@ class SvgNeedleHole extends SvgObj {
 
   create_p_points() {
     let p_points = [];
-    p_points.push(Point(-w / 2,  h * tf));
-    p_points.push(Point( w / 2,  h * (1 - tf)));
-    p_points.push(Point( w / 2, -h * tf));
-    p_points.push(Point(-w / 2, -h * (1 - tf)));
+    p_points.push(new Point(-this.w / 2,  this.h * this.tf));
+    p_points.push(new Point( this.w / 2,  this.h * (1 - this.tf)));
+    p_points.push(new Point( this.w / 2, -this.h * this.tf));
+    p_points.push(new Point(-this.w / 2, -this.h * (1 - this.tf)));
     return p_points;
   }
 }
@@ -230,7 +230,7 @@ class SvgNeedleHole extends SvgObj {
 class Part1 {
   constructor(parent,
 	      w1, w2, h1, h2, bw, bl, bf, dia1, d1, d2,
-	      needle_w, needle_h, needle_tf, needle_corner_rotation) {
+	      needle_w, needle_h, needle_tf, needle_rot) {
     this.parent = parent;
     this.w1 = w1;
     this.w2 = w2;
@@ -245,28 +245,130 @@ class Part1 {
     this.needle_w = needle_w;
     this.needle_h = needle_h;
     this.needle_tf = needle_tf;
-    this.needle_corner_rotation = needle_corner_rotation;
+    this.needle_rot = needle_rot;
 
     // 図形作成
-    this.svg_outline = new SvgPart1Outline(parent,
+    this.svg_outline = new SvgPart1Outline(this.parent,
 					   w1, w2, h1, h2, bw, bl, bf);
-    this.svg_hole = new SvgCircle(parent, this.dia1 / 2);
+
+    this.svg_hole = new SvgCircle(this.parent, this.dia1 / 2);
     let cx = 0;
     let cy = this.h1 + this.h2 + this.bl - this.bw / 2;
     this.p_hole_center = new Point(cx, cy);
+
+    this.vp_needle = this.create_vp_needle();
+    this.svg_needle_hole = [];
+    for (let v of this.vp_needle) {
+      let svg_nh = new SvgNeedleHole(this.parent,
+				     this.needle_w, this.needle_h,
+				     this.needle_tf);
+      this.svg_needle_hole.push([svg_nh, v]);
+    }
   }
 
   draw(origin, stroke_width) {  // origin == [x0, y0, rad0]
     let x0 = origin[0], y0 = origin[1], rad0 = origin[2];
+
+    // アウトライン
     let x = x0, y = y0;
     this.svg_outline.draw([x, y, rad0], "#0000FF", stroke_width);
 
+    // 留め具の穴
     this.p_hole_center.rotate(rad0);
     let cx = x0 + this.p_hole_center.x;
     let cy = y0 + this.p_hole_center.y;
     this.svg_hole.draw([cx, cy, 0], "#FF0000", stroke_width);
+
+    // 針穴
+    for (let i=0; i < this.svg_needle_hole.length; i++) {
+      let svg_nh = this.svg_needle_hole[i][0];
+      let v = this.svg_needle_hole[i][1];
+      let origin_nh = [x0 + v.x, y0 + v.y, v.rad];
+      svg_nh.draw(origin_nh, "#FF0000", stroke_width);
+    }
   }
 
+  create_vp_needle() {
+    let rad1 = Math.atan((this.w2 - this.w1) / (2 * this.h1));
+    let rad1a = (Math.PI - rad1) / 2;
+    let a1 = this.d1 / Math.tan(rad1a);
+
+    let rad2 = (Math.PI / 2) - rad1;
+    let rad2a = (Math.PI - rad2) / 2;
+    let a2 = this.d1 / Math.tan(rad2a);
+
+    let vp1 = [];
+    for (let i=0; i < 6; i++) {
+      let nx = this.svg_outline.p_points[i].x;
+      let ny = this.svg_outline.p_points[i].y;
+      if (i == 0) {
+	nx += this.d1;
+	ny -= this.d1 * 1.5;
+	vp1.push(new Vpoint(nx, ny, 0));
+      }
+      if (i == 1) {
+	nx += this.d1;
+	ny += a1;
+	vp1.push(new Vpoint(nx, ny, rad1));
+      }
+      if (i == 2) {
+	nx += a2;
+	ny += this.d1;
+	vp1.push(new Vpoint(nx, ny, Math.PI / 2));
+      }
+      if (i == 3) {
+	nx -= a2;
+	ny += this.d1;
+	vp1.push(new Vpoint(nx, ny, (Math.PI / 2) + rad2));
+      }
+      if (i == 4) {
+	nx -= this.d1;
+	ny += a1;
+	vp1.push(new Vpoint(nx, ny, Math.PI));
+      }
+      if (i == 5) {
+	nx -= this.d1;
+	ny -= this.d1 * 1.5;
+	vp1.push(new Vpoint(nx, ny, Math.PI));
+      }
+      if (i > 5) {
+	break;
+      }
+    } // for
+
+    // 補完する点を生成
+    let vp2 = [];
+    for (let i=0; i < vp1.length - 1; i++) {
+      let d = vp1[i].distance(vp1[i+1]);
+      let n = Math.abs(Math.round(d / this.d2));
+      vp2 = vp2.concat(this.split_vp(vp1[i], vp1[i+1], n));
+    } // for
+
+    vp2.unshift(vp1[0]);
+    return vp2;
+  }
+
+  split_vp(v1, v2, n) {
+    if (n == 0) {
+      return v1;
+    }
+
+    let dx = (v2.x - v1.x) / n;
+    let dy = (v2.y - v1.y) / n;
+
+    let v = [];
+    for (let i=0; i < n; i++) {
+      v.push(new Vpoint(v1.x + dx * (i + 1),
+			v1.y + dy * (i + 1),
+			v1.rad));
+    }
+
+    if (this.needle_rot) {
+      v.slice(-1)[0].rad = (v1.rad + v2.rad) / 2;
+    }
+
+    return v;
+  }
 }
 
 class Part2 {
@@ -275,6 +377,8 @@ class Part2 {
     this.part1 = part1;
     this.dia2 = dia2;
 
+    // アウトライン
+    //   ``Part1``の``svg_outline.p_points``をミラーして利用
     this.points_outline = [];
     for (let i=0; i < 6; i++) {
       let pp = this.part1.svg_outline.p_points[i];
@@ -283,21 +387,47 @@ class Part2 {
     }
     this.svg_outline = new SvgPolygon(parent, this.points_outline);
 
+    // 留め具の穴
     this.svg_hole = new SvgCircle(parent, this.dia2 / 2);
     let cx = 0;
     let cy = this.part1.h1 + this.part1.h2 - this.svg_hole.r - this.part1.d1;
     this.p_hole_center = new Point(cx, cy);
+
+    // 針穴
+    //   ``Part1``の``vp_needle``をミラーして利用
+    this.svg_needle_hole = [];
+    for (let i=0; i < this.part1.vp_needle.length; i++) {
+      let v = this.part1.vp_needle[i];
+      v.mirror();
+      // ``SvgNeedleHole``もミラーする
+      let svg_nh = new SvgNeedleHole(this.parent,
+				     this.part1.needle_w,
+				     this.part1.needle_h,
+				     this.part1.needle_tf);
+      svg_nh.mirror();
+      this.svg_needle_hole.push([svg_nh, v]);
+    }
   }
 
   draw(origin, stroke_width) {
     let x0 = origin[0], y0 = origin[1], rad0 = origin[2];
 
+    // アウトライン
     this.svg_outline.draw([x0, y0, rad0], "#0000FF", stroke_width);
 
+    // 留め具の穴
     this.p_hole_center.rotate(rad0);
     let cx = x0 + this.p_hole_center.x;
     let cy = y0 + this.p_hole_center.y;
     this.svg_hole.draw([cx, cy, 0], "#FF0000", stroke_width);
+
+    // 針穴
+    for (let i=0; i < this.svg_needle_hole.length; i++) {
+      let svg_nh = this.svg_needle_hole[i][0];
+      let v = this.svg_needle_hole[i][1];
+      let origin_nh = [x0 + v.x, y0 + v.y, v.rad];
+      svg_nh.draw(origin_nh, "#FF0000", stroke_width);
+    }
   }
 }
 
@@ -353,7 +483,7 @@ function gen_svg(id_canvas, id_download) {
   for (let k of opt_list) {
     opt[k] = parseFloat(document.getElementById(k).value);
   }
-  opt.needle_rot = (document.getElementById("needle_rot").value == "true");
+  opt.needle_rot = document.getElementById("needle_rot").checked;
 
   //
   // make objects and draw them
@@ -382,7 +512,7 @@ function gen_svg(id_canvas, id_download) {
 			opt.w1, opt.w2, opt.h1, opt.h2,
 			opt.bw, opt.bl, opt.bf,
 			opt.dia1, opt.d1, opt.d2,
-			opt.neetle_w, opt.neetle_h, opt.neetle_tf,
+			opt.needle_w, opt.needle_h, opt.needle_tf,
 			opt.needle_rot);
   part1.draw([x0, y0, 0], STROKE_WIDTH);
 
